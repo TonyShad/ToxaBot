386d8228d517e0517647d3ef7570ec07915ac606
Anton Barkovskiy <antonbarkovskiy@iMac-Anton.local>
2018-08-24 17:05:40 +0300

    init

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
 
 bot.onText(/\/bidloMode (.+)/, (msg, match) => {
     let id = msg.chat.id;
-    if(match[1] == "true" && _.findIndex(bidloMode, function(o) {return o.chat == id}) == 0) {
+    if(match[1] == "true" && _.findIndex(bidloMode, function(o) {return o.chat == id}) == -1) {
         bidloMode.push({"chat": id, "mode": "on"});
         bot.sendMessage(id, "Bidlo Mode set to True");
     } else {
@@ -38,7 +38,7 @@ bot.onText(/\/sosat/, (msg) => {
 
 setInterval(function(){
         var curDate = new Date();
-        if (curDate.getHours() == 17 && curDate.getMinutes() == 05 && curDate.getSeconds() == 10) {
+        if (curDate.getHours() == 17 && curDate.getMinutes() == 07 && curDate.getSeconds() == 10) {
             bidloMode.map((item, i) => {
                 if(item.mode == "on") {
                     bot.sendMessage(item.chat, "Бля, еще двутораху сидеть")
